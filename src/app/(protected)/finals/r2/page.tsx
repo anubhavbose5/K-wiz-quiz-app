d7ad60e6425c7ba94df7d96023a7b1d2646a0222
Anubhav Bose <anubhav.bose05@gmail.com>
2025-09-23 16:08:54 +0530

    timer on finals round2

--- src/app/(protected)/finals/r2/page.tsx
+++ src/app/(protected)/finals/r2/page.tsx
@@ -3,11 +3,15 @@
 import RoundHeader from "@/components/RoundHeader";
 import QuestionManager from "@/components/QuestionManager";
 import { finalsQuestionsRound3 } from "@/data/finals";
+import CountdownTimer from "@/components/CountDownTImer";
 
 export default function Prelims1Round2Page() {
   return (
     <main className="min-h-screen flex flex-col">
       <RoundHeader title="Mind over Montage" />
+      <div className="fixed top-[15%] right-4 z-50">
+        <CountdownTimer totalSeconds={30} size={120} onComplete={() => {}} />
+      </div>
 
       <div className="flex-1 px-4 py-10">
         <QuestionManager questions={finalsQuestionsRound3} />
